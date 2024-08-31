1e0854f68cd108e8f20d887d8a9123fca67aa8f6
Aitishnitsa <aitishnitsa@gmail.com>
2024-08-31 22:11:06 +0300

    toggle menu button

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { ReactLenis, useLenis } from "lenis/react";
 import logo from "../assets/logo.svg";
-import burgerMenu from "../assets/header/burger_menu.svg";
+// import burgerMenu from "../assets/header/burger_menu.svg";
 import Button from "./Button";
 import useScreenSize from "../hooks/useScreenSize";
 import { useGSAP } from "@gsap/react";
@@ -9,13 +9,19 @@ import gsap from "https://esm.sh/gsap";
 
 const Nav = () => {
   const [showToggleBtn, setShowToggleBtn] = useState(false);
+  const [showMenu, setShowMenu] = useState(true);
   const screenSize = useScreenSize();
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
 
   useEffect(() => {
     if (screenSize.width <= 768) {
       setShowToggleBtn(true);
     } else {
       setShowToggleBtn(false);
+      setShowMenu(true);
     }
   }, [screenSize.width]);
 
@@ -92,9 +98,106 @@ const Nav = () => {
           <a href="#">
             <img src={logo} alt="logo" />
           </a>
-          <button>
-            <img src={burgerMenu} alt="menu button" />
-          </button>
+          <nav className="flex">
+            <div
+              className={`${
+                showMenu && showToggleBtn ? "hidden" : "block"
+              } flex space-x-1 md:space-x-2`}
+            >
+              <nav>
+                <ul className="flex space-x-6 font-semibold text-xxs">
+                  <li>
+                    <a
+                      href="#cases"
+                      className="relative overflow-hidden flex justify-between"
+                    >
+                      <div className="overflow-hidden h-3 flex items-center justify-center flex-col">
+                        <p className="primary w-full translate-y-1/2">CASES</p>
+                        <p className="secondary w-full translate-y-2/3">
+                          CASES
+                        </p>
+                      </div>
+                      <div className="line absolute -bottom-px origin-center w-full scale-0 border border-b-px border-white"></div>
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#it-specialists-section"
+                      className="relative overflow-hidden flex justify-between"
+                    >
+                      <div className="overflow-hidden h-3 flex items-center justify-center flex-col">
+                        <p className="primary w-full translate-y-1/2">TOOLS</p>
+                        <p className="secondary w-full translate-y-2/3">
+                          TOOLS
+                        </p>
+                      </div>
+                      <div className="line absolute -bottom-px origin-center w-full scale-0 border border-b-px border-white"></div>
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#about"
+                      className="relative overflow-hidden flex justify-between"
+                    >
+                      <div className="overflow-hidden h-3 flex items-center justify-center flex-col">
+                        <p className="primary w-full translate-y-1/2">ABOUT</p>
+                        <p className="secondary w-full translate-y-2/3">
+                          ABOUT
+                        </p>
+                      </div>
+                      <div className="line absolute -bottom-px origin-center w-full scale-0 border border-b-px border-white"></div>
+                    </a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+            <button
+              className={showToggleBtn ? "block" : "hidden"}
+              onClick={toggleMenu}
+            >
+              {showMenu ? (
+                <svg
+                  fill="none"
+                  height="28"
+                  viewBox="0 0 28 28"
+                  width="28"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M3 7C3 6.44771 3.44772 6 4 6H24C24.5523 6 25 6.44771 25 7C25 7.55229 24.5523 8 24 8H4C3.44772 8 3 7.55229 3 7Z"
+                    fill="white"
+                  />
+                  <path
+                    d="M3 14C3 13.4477 3.44772 13 4 13H24C24.5523 13 25 13.4477 25 14C25 14.5523 24.5523 15 24 15H4C3.44772 15 3 14.5523 3 14Z"
+                    fill="white"
+                  />
+                  <path
+                    d="M4 20C3.44772 20 3 20.4477 3 21C3 21.5523 3.44772 22 4 22H24C24.5523 22 25 21.5523 25 21C25 20.4477 24.5523 20 24 20H4Z"
+                    fill="white"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  height="28"
+                  version="1.1"
+                  viewBox="0 0 32 32"
+                  width="28"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M17.459,16.014l8.239-8.194c0.395-0.391,0.395-1.024,0-1.414c-0.394-0.391-1.034-0.391-1.428,0  l-8.232,8.187L7.73,6.284c-0.394-0.395-1.034-0.395-1.428,0c-0.394,0.396-0.394,1.037,0,1.432l8.302,8.303l-8.332,8.286  c-0.394,0.391-0.394,1.024,0,1.414c0.394,0.391,1.034,0.391,1.428,0l8.325-8.279l8.275,8.276c0.394,0.395,1.034,0.395,1.428,0  c0.394-0.396,0.394-1.037,0-1.432L17.459,16.014z"
+                    fill="#fff"
+                  />
+                  <g />
+                  <g />
+                  <g />
+                  <g />
+                  <g />
+                  <g />
+                </svg>
+              )}
+            </button>
+          </nav>
         </div>
       ) : (
         <>
